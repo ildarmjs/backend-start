709c36eae784b5cc7adfb445cac4ee1ea90df06d
Ildar <ildar.1795@gmail.com>
2023-10-07 21:18:26 +0400

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-app.get('/home', (req, res) => {
+const products = [
+	{ id: 1, title: 'Tomato', price: 300 },
+	{ id: 2, title: 'Apple', price: 100 }
+]
+
+app.get('/products', (req, res) => {
+	res.send(products)
+})
+app.get('/products/:id', (req, res) => {
+	let product = products.find(p => p.id === +req.params.id)
+	if (product) {
+		res.send(product)
+	} else {
+		res.send(404)
+	}
+})
+
+app.get('/', (req, res) => {
 	res.status(200).json('Welcome, your app is working well')
 })
 
